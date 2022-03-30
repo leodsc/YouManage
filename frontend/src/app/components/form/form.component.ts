dae5fb8275773e28db24691c76d94e49d82981fd
Leonardo <leo@pop-os.localdomain>
2022-03-30 12:23:05 -0300

    finished login input

--- frontend/src/app/components/form/form.component.ts
+++ frontend/src/app/components/form/form.component.ts
@@ -8,10 +8,17 @@ import Label from 'src/app/classes/Label';
 })
 export class FormComponent implements OnInit {
   @Input() data?: Label[];
+  info: string =
+    'O nome do gerente cadastrado na plataforma, caso não exista, crie um.';
+  showInfo: boolean = false;
 
   constructor() {}
 
   ngOnInit(): void {
     console.log(this.data);
+  }
+
+  showIconInfo(): void {
+    this.showInfo = !this.showInfo;
   }
 }
